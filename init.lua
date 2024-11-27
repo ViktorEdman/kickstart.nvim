530803b0d2e01da7b476fedb98131a5b2eaafd44
Viktor Edman <viktor@edman.se>
2024-11-27 13:48:10 +0100

    Added cpp formatting

--- init.lua
+++ init.lua
@@ -739,6 +739,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'gofmt' },
+        cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
